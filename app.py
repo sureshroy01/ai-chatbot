91b0bd13257ccd3c061e04f724599f240c1083ae
sureshroy01 <unanimous627@gmail.com>
2026-03-27 13:03:55 +0530

    Update app.py

--- app.py
+++ app.py
@@ -40,7 +40,7 @@ def chat():
     })
 
 
-if _name_ == "_main_":
+if __name__ == "__main__":
     import os
     port = int(os.environ.get("PORT", 10000))
     app.run(host="0.0.0.0", port=port, debug=True)
